6f51f5bbdc1bb27af89ff1941f0ddc34f7f502fc
Evgenii Pochechuev <ipchchv@gmail.com>
2022-08-05 09:56:50 +0000

    Add a limit to the number of requests (1000 requests/sec)

--- server.go
+++ server.go
@@ -4,9 +4,11 @@ import (
 	"log"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func server() {
 	e := echo.New()
+	e.Use(middleware.RateLimiter(middleware.NewRateLimiterMemoryStore(1000)))
 	log.Fatal(e.Start(envURL))
 }
