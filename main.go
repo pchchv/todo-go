325ba9d76ce9eb206ee45f3ea164149e0d7f64f9
Evgenii Pochechuev <ipchchv@gmail.com>
2022-08-05 09:55:06 +0000

    Implement data retrieval from an .env file

--- main.go
+++ main.go
@@ -1,3 +1,30 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+var envURL string
+
+func init() {
+	// Load values from .env into the system
+	if err := godotenv.Load(); err != nil {
+		log.Panic("No .env file found")
+	}
+}
+
+func getEnvValue(v string) string {
+	value, exist := os.LookupEnv(v)
+	if !exist {
+		log.Panicf("Value %v does not exist", v)
+	}
+	return value
+}
+
+func main() {
+	envURL = getEnvValue("HOST") + ":" + getEnvValue("PORT")
+	server()
+}
