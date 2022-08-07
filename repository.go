e81cc1d92bdf429f23f594f0efaa2754311357e0
pchchv <ipchchv@gmail.com>
2022-08-07 13:36:22 +0300

    Implement the interface methods

--- repository.go
+++ repository.go
@@ -1,5 +1,7 @@
 package main
 
+import "errors"
+
 type TodoRepository interface {
 	Create(todo *Todo)
 	GetAll() []*Todo
@@ -19,4 +21,47 @@ func NewInMemoryTodoRepository() TodoRepository {
 	t.Todos = make([]*Todo, 0)
 	t.nextId = 1
 	return t
+}
+
+func (r *InMemoryTodoRepository) Create(todo *Todo) {
+	todo.Id = r.nextId
+	r.Todos = append(r.Todos, todo)
+	r.nextId++
+}
+
+func (r *InMemoryTodoRepository) GetAll() []*Todo {
+	return r.Todos
+}
+
+func (r *InMemoryTodoRepository) DeleteAll() {
+	r.Todos = make([]*Todo, 0)
+}
+
+func (r *InMemoryTodoRepository) Get(id int) (t *Todo, err error) {
+	for _, t = range r.Todos {
+		if t.Id == id {
+			return t, nil
+		}
+	}
+	return nil, errors.New("todo not found")
+}
+
+func (r *InMemoryTodoRepository) Delete(id int) (err error) {
+	for i, t := range r.Todos {
+		if t.Id == id {
+			r.Todos = append(r.Todos[:i], r.Todos[i+1:]...)
+			return nil
+		}
+	}
+	return errors.New("todo not found")
+}
+
+func (r *InMemoryTodoRepository) Update(todo *Todo) (err error) {
+	for i, t := range r.Todos {
+		if t.Id == todo.Id {
+			r.Todos[i] = todo
+			return nil
+		}
+	}
+	return errors.New("todo not found")
 }
